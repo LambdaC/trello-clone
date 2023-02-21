e8e5e2bf2b5e1887fcb2831853c19f2f72632c68
LambdaC <studycwg@gmail.com>
2023-02-21 21:31:05 +0800

    create styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { AppContainer } from './style';
+import { ColumnContainer } from './style';
+import { ColumnTitle } from './style';
+import { CardContainer } from './style';
 
 export function App() {
   return (
-    <div></div>
+    <AppContainer>
+      <ColumnContainer>
+        <ColumnTitle>标题</ColumnTitle>
+        <CardContainer>内容</CardContainer>
+      </ColumnContainer>
+    </AppContainer>
   );
 }
