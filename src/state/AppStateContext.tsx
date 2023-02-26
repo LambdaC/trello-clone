e0819efe8fa6d2564b3393ddf52b8f1cd6cc6535
LambdaC <studycwg@gmail.com>
2023-02-26 15:47:47 +0800

    add useReducer

--- src/state/AppStateContext.tsx
+++ src/state/AppStateContext.tsx
@@ -1,19 +1,7 @@
-import { createContext, FC, useContext } from "react"
+import { createContext, Dispatch, FC, useContext, useReducer } from "react"
+import { Action } from "./actions"
+import { AppState, appStateReducer, List, Task } from "./appStateReducer"
 
-type Task = {
-    id: string,
-    text: string,
-}
-
-type List = {
-    id: string,
-    text: string,
-    tasks: Task[]
-}
-
-export type AppState = {
-    lists: List[]
-}
 
 const appData: AppState = {
     lists: [
@@ -38,19 +26,22 @@ const appData: AppState = {
 type AppStateContextProps = {
     lists: List[],
     getTasksByListId(id: string): Task[]
+    dispatch: Dispatch<Action>
 }
 
 const AppStateContext = createContext<AppStateContextProps>({} as AppStateContextProps);
 
 export const AppStateProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
-    const { lists } = appData;
+    const [state, dispatch] = useReducer(appStateReducer, appData);
+
+    const { lists } = state;
 
     const getTasksByListId = (id: string) => {
         return lists.find((list) => list.id === id)?.tasks || []
     }
 
     return (
-        <AppStateContext.Provider value={{ lists, getTasksByListId }}>
+        <AppStateContext.Provider value={{ lists, getTasksByListId, dispatch }}>
             {children}
         </AppStateContext.Provider>
     )
